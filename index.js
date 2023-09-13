bd055786ad33bfa07f84cc021653f83729582b43
Emile <emile00013@gmail.com>
2023-09-13 18:04:24 +0200

    Save task by form

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const node_datetime = require('node-datetime');
+
 require('dotenv').config();
 Task = require('./Model/Task.js');
 // User = require('./Model/User.js');
@@ -11,6 +13,11 @@ const sequelize = require('./db.js');
 
 //app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
 app.listen(port, () => {
 
@@ -43,13 +50,18 @@ app.get('/api/task', (req, res) => {
 
 app.post('/api/task', (req, res) => {
 
-    const requestBody = req.body;
-    
+    // const requestBody = req.body;
+
+    var dateInteger = node_datetime.create();
+    var createdAt = dateInteger.format('m/d/Y H:M:S');
+
+    console.log(req);
+
     const newTask = Task.create ({
-        title: requestBody.title,
-        content: requestBody.content,
-        created_at: requestBody.created_at,
-        is_done: requestBody.is_done
+        title: req.body.title,
+        content: req.body.content,
+        created_at: createdAt,
+        is_done: false
 
     }).catch(error => {
 
